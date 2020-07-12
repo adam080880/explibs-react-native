210072e3254a5e5e937e17c443a5277d07a638a2
adam080880 <muhamadadam20@gmail.com>
2020-07-12 11:35:47 +0700

    add splash screen

--- App.js
+++ App.js
@@ -9,6 +9,10 @@ import Home from './src/screens/Main';
 import {Provider} from 'react-redux';
 import MainStore from './src/redux/store';
 import {PersistGate} from 'redux-persist/integration/react';
+import {Image, View} from 'react-native';
+import Spinner from 'react-native-spinkit';
+import typography from './src/styles/typography';
+import color from './src/styles/color';
 
 const Stack = createStackNavigator();
 
@@ -17,8 +21,14 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       isLogin: false,
-      page: 'home',
+      page: 'splash',
     };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.changePage('home');
+    }, 2000);
   }
 
   changePage = (page) => {
@@ -31,6 +41,37 @@ export default class App extends React.Component {
     login: (props) => <Login {...props} changePage={this.changePage} />,
     register: (props) => <Register {...props} changePage={this.changePage} />,
     home: (props) => <Home {...props} changePage={this.changePage} />,
+    splash: (props) => (
+      <View
+        style={{
+          ...{
+            flex: 1,
+            backgroundColor: 'white',
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+        }}>
+        <Image
+          style={{...{width: 150, height: 150}}}
+          source={require('./src/assets/images/explibs-app.png')}
+        />
+        <Spinner
+          style={{
+            ...{
+              fontWeight: 'bold',
+              fontSize: typography.FONT_SIZE_LABEL,
+              color: color.COLOR_FONT_PLAIN,
+              marginVertical: 27,
+            },
+            ...{opacity: 0.6},
+          }}
+          isVisible={true}
+          size={typography.FONT_SIZE_APP * 3}
+          type={'Pulse'}
+          color={color.COLOR_UTILITIES_BACKGROUND}
+        />
+      </View>
+    ),
   };
 
   render() {
@@ -64,6 +105,15 @@ export default class App extends React.Component {
                     headerShown: false,
                   }}
                   component={this.page.home}
+                />
+              )}
+              {this.state.page === 'splash' && (
+                <Stack.Screen
+                  name={'home'}
+                  options={{
+                    headerShown: false,
+                  }}
+                  component={this.page.splash}
                 />
               )}
             </Stack.Navigator>
